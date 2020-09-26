075663db13bc3be4b92c66b24fc76c46a4a6ae96
ashish ghimire <ashish.gh123@gmail.com>
2020-09-26 09:27:14 +0545

    Pytesseract implementation and converting text to dataframe

--- app/app.py
+++ app/app.py
@@ -9,6 +9,11 @@ from loguru import logger
 from flask import Flask, abort, jsonify, request, g
 
 
+from .parser.pytesseract import parse_image
+
+
+
+ALLOWED_TYPES = [".jpg", ".png", ".jpeg"]
 
 app = Flask(__name__)
 logger.info("starting server . . .")
@@ -18,10 +23,52 @@ if not os.path.exists("tmp"):
     os.makedirs("tmp")
 
 
-
 @app.route("/api/v1/extract/", methods=["POST"])
 def extract():
+    logger.info("extracting data from image")
+
+    if not request.headers["Content-Type"].startswith("multipart/form-data"):
+        abort_json(400, "Content-Type can only be json or form-data")
+
+    if not request.files:
+        print("no files")
+        abort_json(400, "IMAGE_MISSING_ON_REQUEST")
+        
+    logger.debug(f"Request file names {request.files}")
+    file = request.files["file"]
+
+    _, ext = os.path.splitext(os.path.basename(file.filename))
+    ext = ext.lower()
+
+    if ext not in ALLOWED_TYPES:
+        print("no allowed types")
+        abort(400, "File Type Not Supported", f"Allowed file types are : {ALLOWED_TYPES}")
+    
+    fid = uuid.uuid4().hex
+    file_name = os.path.join("tmp", fid+ext)
+    file.save(file_name)
+
+    logger.info(f"File saved as : {file_name}")
+
+    # TODO:
+    # 1. call to function to extract data from image
+    if file_name:
+        try:
+            # TODO            
+            # First get dataframe for image
+            # then create simple extraction part
+            df = parse_image(file_name, debug=False)
+            if not df.empty:
+                for row in df.head().itertuples():
+                    print(row)
+                    
+                # print(df.head())                
+        except Exception as e:
+            logger.error(f"dataframe extraction error : {e}")
+    else:
+        print("No file ")
     return "Hello, from server"
+
 
 
 
---
+++ app/parser/pytesseract.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import time
+import cv2
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pytesseract
+from pytesseract import Output
+
+from PIL import Image
+
+def parse_image(file_name: str, debug:bool = True)-> pd.DataFrame:
+    """
+    Converts image to dataframe
+
+    Args:
+        ``file_name``: file_name of image
+    
+    Return
+        ``df``: pd.DataFrame
+            dataframe of text from image
+    """
+    if not file_name:
+        return pd.DataFrame([])
+    
+    image = cv2.imread(file_name)
+
+    if debug:
+        img = Image.open(file_name).copy()
+        img.show()
+    
+    try:
+        extracted_data = pytesseract.image_to_data(image, output_type= Output.DICT)
+    except Exception as e:
+        logger.error(f"Error on pytesseract : {e}")
+
+    # possible keys
+    # ['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text'])
+
+    data = []
+    for i in range(len(extracted_data["text"])):
+        if int(extracted_data["conf"][i]) > 60:
+            tmp_data = {
+                "level": extracted_data["level"][i],
+                "page_num": extracted_data["page_num"][i],
+                "block_num": extracted_data["block_num"][i],
+                "par_num": extracted_data["par_num"][i],
+                "line_num": extracted_data["line_num"][i],
+                "word_num": extracted_data["word_num"][i],
+                "x0": extracted_data["left"][i],
+                "y0": extracted_data["top"][i],
+                "x2": extracted_data["width"][i],
+                "y2": extracted_data["height"][i],
+                "Text": extracted_data["text"][i],
+                "conf": extracted_data["conf"][i],
+                }
+            data.append(tmp_data)
+    
+    if data:
+        df = pd.DataFrame(data)
+        # TODO:
+        # Once data is extracted, there may be unwanted datas as well so make sure these datas are also eliminated during the post processing stage.
+        # make function in utils/ clean_dataframe(df)-> df
+
+    return df
+
+
+
